ace5238b8f10ccc9cdbdb2256a9a25d0b9e57dce
Gianluca <gianluca.marin@students.fhnw.ch>
2025-06-05 12:40:35 +0200

    Analytics Manager hinzugefügt

--- business_logic/__init__.py
+++ business_logic/__init__.py
@@ -4,5 +4,6 @@ from .guest_manager import GuestManager
 from .hotel_manager import HotelManager
 from .invoice_manager import InvoiceManager
 from .pricing_manager import PricingManager
+from .analytics_manager import AnalyticsManager
 
 # Zentrale Importdatei für alle Manager-Klassen (Business Logic Layer) 
